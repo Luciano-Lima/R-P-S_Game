97d194aeea5dde9dc84c237a55a0f56ddb7fa82f
Luciano-Lima <ccll4ever1@gmail.com>
2022-05-31 10:38:39 +0100

    Added play game function

--- assets/js.js
+++ assets/js.js
@@ -1,16 +1,41 @@
-const selection = document.querySelectorAll('[data-selection]')
+const selectBtn = document.querySelectorAll('[data-selection]')
+
+let userPick
 
 // selects user choice
-selection.forEach((selection) => {
-    selection.addEventListener('click', () => {
-        const userPick = selection.dataset.selection
+selectBtn.forEach((e) => {
+    e.addEventListener('click', () => {
+        userPick = e.dataset.selection //dataset.selection selects user option
+        playGame(userPick)
     })
 })
 
 // selects computer choice
-function compSelection() {
-    const options = ['rock', 'paper', 'scissors']
-    const randonSelection = Math.floor(Math.random() * options.length)
+const computerPick = () => {
+    const options = ['rock', 'paper', 'scissors'];
+    const randonSelection = Math.floor(Math.random() * options.length) //returns random computer selection based in the options given
     return options[randonSelection]
 }
-compSelection()
+
+// Play game
+const playGame = (userPick) => {
+    let compPick = computerPick()
+    switch (userPick + compPick) {
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
+            console.log('draw')
+            break
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
+            console.log('userWin')
+            break
+        case 'rockpaper':
+        case 'paperscissors':
+        case 'scissorsrock':
+            console.log('computer wins')
+            break
+    }
+}
+playGame()
